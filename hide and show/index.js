c1f497f58b611d2e17369d2c3d48ff5eece5f11c
Ruby Ghosh <60173326+ghoshruby@users.noreply.github.com>
2021-02-20 20:25:35 +0530

    Add files via upload

---
+++ hide and show/index.js
@@ -0,0 +1,119 @@
+
+
+$(document).ready(function()
+{
+    $("#food").click(function()
+    {
+        $(".button1").show();
+        $(".button2").hide();
+         $(".button3").hide();
+    });
+});
+
+$(document).ready(function()
+{
+    $("#flower").click(function()
+    {
+        $(".button2").show();
+         $(".button1").hide();
+          $(".button3").hide();
+    });
+});
+
+$(document).ready(function()
+{
+    $("#animal").click(function()
+    {
+        $(".button3").show();
+         $(".button1").hide();
+          $(".button2").hide();
+    });
+});
+
+$(document).ready(function()
+{
+    $("#biri").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='biriyani.jfif'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
+
+$(document).ready(function()
+{
+    $("#fuc").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='fuc.jpg'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
+
+$(document).ready(function()
+{
+    $("#roll").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='roll.JPG'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
+
+$(document).ready(function()
+{
+    $("#ros").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='ros.png'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
+
+$(document).ready(function()
+{
+    $("#sun").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='sun.jpg'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
+
+$(document).ready(function()
+{
+    $("#jas").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='jas.jfif'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
+$(document).ready(function()
+{
+    $("#lion").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='lion.jpg'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
+
+$(document).ready(function()
+{
+    $("#hor").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='horse.jfif'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
+$(document).ready(function()
+{
+    $("#tig").click(function()
+    {
+        document.getElementById("foodpic").innerHTML="<img src ='tiger.jfif'>";
+       
+        // document.write("<img src ='biriyani.jfif'>");
+    });
+});
